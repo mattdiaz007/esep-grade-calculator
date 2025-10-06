585e4fbab6b5875a1853dd0f104ea0e39a3f6368
Matt <matthewdiaz1@ufl.edu>
2025-10-05 20:42:36 -0400

    Fix GradeCalculator weighting and essay average bug; all tests passing

--- grade-calculator/grade_calculator.go
+++ grade-calculator/grade_calculator.go
@@ -80,19 +80,22 @@ func (gc *GradeCalculator) AddGrade(name string, grade int, gradeType GradeType)
 func (gc *GradeCalculator) calculateNumericalGrade() int {
 	assignment_average := computeAverage(gc.assignments)
 	exam_average := computeAverage(gc.exams)
-	essay_average := computeAverage(gc.exams)
+	essay_average := computeAverage(gc.essays)
 
 	weighted_grade := float64(assignment_average)*.5 + float64(exam_average)*.35 + float64(essay_average)*.15
 
 	return int(weighted_grade)
 }
 
-func computeAverage(grades []Grade) int {
+func computeAverage(grades []Grade) float64 {
+	if len(grades) == 0 {
+		return 0
+	}
 	sum := 0
 
-	for grade, _ := range grades {
-		sum += grade
+	for _, grade := range grades {
+		sum += grade.Grade
 	}
 
-	return sum / len(grades)
+	return float64(sum) / float64(len(grades))
 }
